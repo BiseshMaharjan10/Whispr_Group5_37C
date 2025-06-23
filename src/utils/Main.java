e0a0d9faa936b236980f6d2a0f998e584e2206e7
Sujal3022255 <sujalpurbey65@gmail.com>
2025-06-23 17:25:51 +0545

    Enhance Main class to require username input for ClientGui initialization

--- src/utils/Main.java
+++ src/utils/Main.java
@@ -1,16 +1,22 @@
 package utils;
 import view.ClientGui;
+import Controller.SigninController;
 
 import javax.swing.*;
 
 public class Main {
 public static void main(String[] args) {
-    SwingUtilities.invokeLater(() -> {
-        ClientGui gui = new ClientGui();
-        gui.setVisible(true);
-    });
+     String username = JOptionPane.showInputDialog("Enter your username:");
+     if (username != null && !username.trim().isEmpty()) {
+        SwingUtilities.invokeLater(() -> new ClientGui(username).setVisible(true));
+     }
+     else{
+        JOptionPane.showMessageDialog(null, "Username is required.");
+        System.exit(0);
+     }
+    
 }
-}
+    }
 
 
 
